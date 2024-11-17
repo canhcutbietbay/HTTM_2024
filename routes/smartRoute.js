c1f5e598d71c854094c54a6874e428e6f02154dd
canhcutbietbay <huynhtan131517@gmail.com>
2024-11-17 18:02:22 +0700

    init project

--- routes/smartRoute.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as smartController from "../controller/smartController.js"
-import { requireAuth } from "../middleware/authMiddleware.js";
-
-const route = Router()
-
-route.get("/smart", requireAuth, smartController.get)
-
-export default route
